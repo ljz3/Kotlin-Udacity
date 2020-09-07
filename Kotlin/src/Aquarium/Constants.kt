0b62bd4bfe80cd07dd2ef632b459bd0a96bff9e4
ljz3 <kevin.ottawa@hotmail.com>
2020-09-07 17:02:05 -0400

    Playing around with constants and companion objects

---
+++ Kotlin/src/Aquarium/Constants.kt
@@ -0,0 +1,17 @@
+package Aquarium
+
+const val rocks = 3
+
+val number = 5
+
+const val num = 5
+
+const val CONSTANT = 10
+
+val VALUE = {CONSTANT * 5}
+
+class MyClass{
+    companion object{
+        const val CONSTANT3 = "constant inside companion"
+    }
+}
